b0494b2ff53e3407d5d04d0d50dd1d7db4e8228e
Amol Jagtap <amoljagtap17@gmail.com>
2020-10-07 20:12:27 +0530

    P02_VEHICLESAPI: Add test for pricing service

---
+++ pricing-service/src/test/java/com/udacity/pricing/web/PricingControllerUnitTest.java
@@ -0,0 +1,27 @@
+package com.udacity.pricing.web;
+
+import com.udacity.pricing.api.PricingController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getPrice() throws Exception {
+        mockMvc
+            .perform(get("/services/price?vehicleId=2"))
+            .andExpect(status().isOk());
+    }
+}
