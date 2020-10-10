1e15137a0c15eebe4ad9e33d14d9d54accfa4b42
Amol Jagtap <amoljagtap17@gmail.com>
2020-10-10 20:44:37 +0530

    P02_VEHICLESAPI: Update tests for PricingController

--- pricing-service/src/test/java/com/udacity/pricing/web/PricingControllerUnitTest.java
+++ pricing-service/src/test/java/com/udacity/pricing/web/PricingControllerUnitTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -22,6 +23,8 @@ public class PricingControllerUnitTest {
     public void getPrice() throws Exception {
         mockMvc
             .perform(get("/services/price?vehicleId=2"))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.currency").value("USD"))
+            .andExpect(jsonPath("$.vehicleId").value("2"));
     }
 }
